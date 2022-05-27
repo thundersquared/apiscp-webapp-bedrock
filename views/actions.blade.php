3abe31920ea81e692aa0ce223a697814e9e60d82
Anatoli Nicolae <an@thundersquared.com>
2022-05-27 22:48:57 +0200

    Fix actions template
    Signed-off-by: Anatoli Nicolae <an@thundersquared.com>

--- views/actions.blade.php
+++ views/actions.blade.php
@@ -1,37 +1,37 @@
-<button name="packageManager" data-note-target="#packageManager" type="submit" data-toggle="collapse" data-target="#packageManager"
-        aria-controls="#packageManager" aria-expanded="false"
-        class="mb-3 btn btn-secondary " name="packageManager" value="1">
-	<i class="fa fa-gift"></i>
-	Manage Packages
+<button name="packageManager" data-note-target="#packageManager" type="submit" data-toggle="collapse"
+    data-target="#packageManager" aria-controls="#packageManager" aria-expanded="false" class="mb-3 btn btn-secondary "
+    name="packageManager" value="1">
+    <i class="fa fa-gift"></i>
+    Manage Packages
 </button>
 
 <div class="inline-block mb-3 btn btn-secondary" id="wpSsoPlaceholder">
-	<i class="ui-ajax-loading ui-ajax-indicator"></i>
-	{{ _("SSO Check") }}
+    <i class="ui-ajax-loading ui-ajax-indicator"></i>
+    {{ _('SSO Check') }}
 </div>
 
 <table class="table table-responsive">
-	<thead>
-	<th>
-		Name
-	</th>
-	<th>
-		Status
-	</th>
-	</thead>
+    <thead>
+        <th>Name</th>
+        <th>Status</th>
+    </thead>
     <tbody>
-	@php
-		$environments = \cmd('bedrock_get_environments', $app->getHostname(), $app->getPath());
-	@endphp
-	@foreach ($environments as $environment)
-        <tr class="environment-row">
-            <td>
-                {{ $environment['name'] }}
-            </td>
-            <td>
-                {{ $environment['status'] }}
-            </td>
-        </tr>
-	@endforeach
-	</tbody>
+        @php
+            $environments = \cmd('bedrock_get_environments', $app->getHostname(), $app->getPath());
+        @endphp
+        @foreach ($environments as $environment)
+            <tr class="environment-row">
+                <td>
+                    <code>{{ $environment['name'] }}</code>
+                </td>
+                <td>
+                    @if ($environment['status'] === true)
+                        <label class="badge badge-success">Currently Enabled</label>
+                        @elseif
+                        <button class="btn btn-primary" bedrock-action="enabled-environment">Activate</button>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
 </table>
